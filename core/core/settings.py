3f474a27cc6031452020017d7396d3d54f67a99c
Nurbek029 <nurbeksagynbekov@gmail.com>
2025-03-05 16:28:52 +0600

    01.03.2025

--- core/core/settings.py
+++ core/core/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = 'media/'
 MEDIA_URL = '/media/'
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- core/user/views.py
+++ core/user/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+def user_register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегались!')
+            return redirect('index')
+
+        for field, errors in form.errors.items():
+            for error in errors:
+                messages.error(request, f'{error}')
+
+    form = UserRegisterForm()
+    return render(request, 'account/user_register.html', {"form": form})
+
+def user_login_view(request):
+    if request.method == "POST":
+        user_email = request.POST['email']
+        user_password = request.POST['password']
+
+        user = authenticate(request, username=user_email, password=user_password)
+
+        if user:
+            login(request, user)
+            messages.success(request, 'Вы успешно вошли в систему')
+            return redirect('index')
+        else:
+            messages.error(request, 'Неверный логин или пароль')
+
+    return render(request, 'account/user_login.html')
+
+def user_logout_view(request):
+    logout(request)
+    messages.success(request, 'Вы успешно вышли из системы')
+    return redirect('index')
